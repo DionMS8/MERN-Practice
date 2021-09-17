bf333d20899dc9a2b1632570a211cbc639fc9ea2
Dion Singh <dionsingh8@hotmail.com>
2021-09-17 00:35:31 -0400

    Added the HTTP Module to Node and Started Express

--- node.js
+++ node.js
@@ -13,6 +13,30 @@ console.log(`Free Memory: ${freeMemory}`)
 
 
 
-//------------------------------------------------------------------------
+//-----[HTTP MODULE]-------------------------------------------------------------------
+
+// LOADING THE HTTP MODULE
+const http = require("http");
+
+// CREATE A SERVER
+const server = http.createServer();
+
+// ADD AN EVENT FOR WHEN THE CONNECTION IS MADE
+server.on("connection", (socket) => {
+    console.log("New connection...");
+});
+
+// ADD AN EVENT LISTENER ON PORT 3000
+server.listen(3000);
+
+// PRINT THAT THE SERVER IS LISTENING
+console.log("Listening on Port 3000...");
+
+// Based on the method used, response is what the server is instructed to do while request is related to what the client asks the server to do.
+
+
+//--------------------------------------------------------------------------------------
+
+
 
 
